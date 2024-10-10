165bcffae0e50d7d8429427fc04a798ca81ccaf4
Alex <alex2323official@gmail.com>
2024-10-10 12:22:41 +0200

    setup for work

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 import "./style.css";
 
 class FormValidator {
+  #form = document.querySelector("form");
   #emailInput = document.querySelector("#email");
   #countryInput = document.querySelector("#country");
   #zipcodeInput = document.querySelector("#zipCode");
   #passwordInput = document.querySelector("#password");
   #confirmPasswordInput = document.querySelector("#confirmPassword");
   #errorDisplaySpan = document.querySelector("#errorDisplay");
+
+  controllerUserFormInput() {
+    this.#form.addEventListener("input", (event) => {
+      console.log(event);
+    });
+  }
 }
+
+// APP STARTS HERE
+const formValidator = new FormValidator();
+
+formValidator.controllerUserFormInput();
