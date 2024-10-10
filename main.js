c48280c93ecb5c25e10ecbe5e18948e094a245ec
Alex <alex2323official@gmail.com>
2024-10-10 12:02:57 +0200

    Initial commit uploading basics

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+class FormValidator {
+  #emailInput = document.querySelector("#email");
+  #countryInput = document.querySelector("#country");
+  #zipcodeInput = document.querySelector("#zipCode");
+  #passwordInput = document.querySelector("#password");
+  #confirmPasswordInput = document.querySelector("#confirmPassword");
+  #errorDisplaySpan = document.querySelector("#errorDisplay");
+}
